f3a376331e4b0ee2e40b64befc6777de2b623570
Székely Mátyás <szekely.m4ty4s@gmail.com>
2023-06-01 12:16:05 +0200

    atoi ok

--- src/8.string-to-integer-atoi.rs
+++ src/8.string-to-integer-atoi.rs
@@ -4,17 +4,61 @@
  * [8] String to Integer (atoi)
  */
 
+
+
 // @lc code=start
+use std::convert::TryFrom;
+
 impl Solution {
     pub fn my_atoi(s: String) -> i32 {
-        let mut num: i32 = 0;
+        let len: usize = s.len();
+        let mut num: i64 = 0;
         let mut sign: i32 = 1;
-        for c in s.bytes()
-        {
-            c match {
-                
-            }
+        let mut c: u8 = 0;
+        let mut i: usize = 0;
+        let vec: Vec<u8>= s.bytes().collect();
+        while (i < len){
+            c = vec[i];
+            i += 1;
+            match c {
+                b' ' => { continue; }
+                b'+' => { break; } 
+                b'-' => {
+                    sign = -1;
+                    break;
+                } 
+                n @ b'0'..=b'9' => {
+                    num += i64::try_from(n - b'0').unwrap();
+                    break;
+                }
+                _ => { return 0; }
+            };
         }
+        while (i < len){
+            c = vec[i];
+            i += 1;
+            match c {
+                n @ b'0'..=b'9' => {
+                    num = num * 10 + i64::try_from(n - b'0').unwrap();
+                    if(num > i32::MAX.into()){
+                        match (sign) {
+                            1 => { return i32::MAX },
+                            -1 => { return i32::MIN },
+                            _ => { panic!("Unreachable") }
+                        };
+                    }
+                    continue;
+                }
+                _ => { break; }
+            };
+        }
+        if(sign == -1){
+            num *= -1;
+        }
+        if(num < i32::MIN.into()){
+            return i32::MIN;
+        }
+        return i32::try_from(num).unwrap();
     }
 }
 // @lc code=end
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
 fn main() {
     println!("Hello, world!");
+    let s: String = String::from("Hello, world!");
+    let s: Vec<u8> = s.bytes().collect();
+    let mut num = 0;
+    let mut i = 0;
+    while (i < s.len())
+    {
+        let c = s[i];
+        match c {
+            b' ' => { continue;}
+            b'+' => { }
+        }
+    }
+    return num;
 }
